a4fe2ec5eef27f7b6a6a7890f5fa1f3c0da703a7
Sara Jensen <jensen.sara.e@gmail.com>
2016-11-18 15:51:47 -0800

    Add clocks to every page, add background image and change font on clothing suggestion activity

--- app/src/main/java/com/epicodus/ransroad/ClothingActivity.java
+++ app/src/main/java/com/epicodus/ransroad/ClothingActivity.java
@@ -1,13 +1,24 @@
 package com.epicodus.ransroad;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ClothingActivity extends AppCompatActivity {
+    @Bind(R.id.clothingTitleTextView) TextView mClothingTitleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clothing);
+        setContentView(R.layout.activity_weather);
+        ButterKnife.bind(this);
+
+        Typeface seasideFont = Typeface.createFromAsset(getAssets(), "fonts/seaside_font.ttf");
+        mClothingTitleTextView.setTypeface(seasideFont);
     }
+
 }
