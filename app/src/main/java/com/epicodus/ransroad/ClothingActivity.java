abb3e3bacc1155ddc3b05d1b039463bb6f292df1
Sara Jensen <jensen.sara.e@gmail.com>
2016-11-18 13:53:25 -0800

    (Re)start android project.

---
+++ app/src/main/java/com/epicodus/ransroad/ClothingActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.ransroad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClothingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothing);
+    }
+}
