74a110842662241e2cb22c593ba30f4ed5f4cf50
Sara Jensen <jensen.sara.e@gmail.com>
2016-11-18 15:04:10 -0800

    confirm user input for lat and long recieved from homepage, get weather button transitions to weather activity.

--- app/src/main/java/com/epicodus/ransroad/MainActivity.java
+++ app/src/main/java/com/epicodus/ransroad/MainActivity.java
@@ -1,13 +1,43 @@
 package com.epicodus.ransroad;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.titleTextView) TextView mTitleTextView;
+    @Bind(R.id.getWeatherButton) Button mGetWeatherButton;
+    @Bind(R.id.latitudeEditText) EditText mLatitudeEditText;
+    @Bind(R.id.longitudeEditText) EditText mLongitudeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface seasideFont = Typeface.createFromAsset(getAssets(), "fonts/seaside_font.ttf");
+        mTitleTextView.setTypeface(seasideFont);
+
+        mGetWeatherButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String latitude = mLatitudeEditText.getText().toString();
+        String longitude = mLongitudeEditText.getText().toString();
+        Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+        intent.putExtra("latitude", latitude);
+        intent.putExtra("longitude", longitude);
+        startActivity(intent);
     }
 }
