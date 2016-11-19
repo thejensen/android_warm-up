acb42fe2502a41017b78891e82e90598861f8fc7
Sara Jensen <jensen.sara.e@gmail.com>
2016-11-18 17:11:32 -0800

    Pass lat and long input on the homepage and send them to the weather page.

--- app/src/main/java/com/epicodus/ransroad/WeatherActivity.java
+++ app/src/main/java/com/epicodus/ransroad/WeatherActivity.java
@@ -1,16 +1,20 @@
 package com.epicodus.ransroad;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class WeatherActivity extends AppCompatActivity {
-    @Bind(R.id.weatherTitleTextView)
-    TextView mWeatherTitleTextView;
+public class WeatherActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.weatherTitleTextView) TextView mWeatherTitleTextView;
+    @Bind(R.id.weatherTextView) TextView mWeatherTextView;
+    @Bind(R.id.getClothingButton) Button mGetClothingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,18 @@ public class WeatherActivity extends AppCompatActivity {
 
         Typeface seasideFont = Typeface.createFromAsset(getAssets(), "fonts/seaside_font.ttf");
         mWeatherTitleTextView.setTypeface(seasideFont);
+
+        Intent intent = getIntent();
+        String latitude = intent.getStringExtra("latitude");
+        String longitude = intent.getStringExtra("longitude");
+        mWeatherTextView.setText("This would be the weather information for " + latitude + ", " + longitude);
+
+        mGetClothingButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(WeatherActivity.this, ClothingActivity.class);
+        startActivity(intent);
     }
 }
