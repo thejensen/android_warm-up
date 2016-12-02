dd26a4334c423e83a4d59e0bd19e892aca711c77
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-02 10:14:53 -0800

    refactor files to put activities into ui packages, model into model package. Update Android Manifest, and layouts to reflect changes.

--- app/src/main/java/com/epicodus/ransroad/ui/ClothingActivity.java
+++ app/src/main/java/com/epicodus/ransroad/ui/ClothingActivity.java
@@ -1,9 +1,8 @@
-package com.epicodus.ransroad;
+package com.epicodus.ransroad.ui;
 
 import android.graphics.Typeface;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
--- app/src/main/java/com/epicodus/ransroad/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/ransroad/ui/MainActivity.java
@@ -1,10 +1,9 @@
-package com.epicodus.ransroad;
+package com.epicodus.ransroad.ui;
 
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
