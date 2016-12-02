a586c769c6dc1bd920ba2335a8cc810a6a7d6043
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-02 10:24:05 -0800

    add Dark Sky API key to gradle properties, pull it out in Constants.java, and include credentials in BuildConfig file by building it in gradle build.

---
+++ app/src/main/java/com/epicodus/ransroad/Constants.java
@@ -0,0 +1,11 @@
+package com.epicodus.ransroad;
+
+import com.epicodus.ransroad.ui.BuildConfig;
+
+/**
+ * Created by jensese on 12/2/16.
+ */
+
+public class Constants {
+    public static final String DARK_SKY_API_KEY = BuildConfig.DARK_SKY_API_KEY;
+}
