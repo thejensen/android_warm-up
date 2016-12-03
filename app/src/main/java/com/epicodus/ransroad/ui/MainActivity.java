caf3e0218923af640798e80c234e6a0e92a864df
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-02 17:21:11 -0800

    add link to darksky.net/poweredby on main activity, per dark sky terms of use.

--- app/src/main/java/com/epicodus/ransroad/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/ransroad/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.epicodus.ransroad.ui;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Bind(R.id.getWeatherButton) Button mGetWeatherButton;
     @Bind(R.id.latitudeEditText) EditText mLatitudeEditText;
     @Bind(R.id.longitudeEditText) EditText mLongitudeEditText;
+    @Bind(R.id.poweredByTextView) TextView mPoweredByTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +30,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mTitleTextView.setTypeface(seasideFont);
 
         mGetWeatherButton.setOnClickListener(this);
+        mPoweredByTextView.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        String latitude = mLatitudeEditText.getText().toString();
-        String longitude = mLongitudeEditText.getText().toString();
-        if(mLatitudeEditText.getText().toString().length() == 0) {
-            mLatitudeEditText.setError("Latitude is required");
+        if (v == mGetWeatherButton) {
+            String latitude = mLatitudeEditText.getText().toString();
+            String longitude = mLongitudeEditText.getText().toString();
+            if(mLatitudeEditText.getText().toString().length() == 0) {
+                mLatitudeEditText.setError("Latitude is required");
+            }
+            else if(mLongitudeEditText.getText().toString().length() == 0) {
+                mLongitudeEditText.setError("Longitude is required");
+            }
+            else {
+                Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+                intent.putExtra("latitude", latitude);
+                intent.putExtra("longitude", longitude);
+                startActivity(intent);
+            }
         }
-        else if(mLongitudeEditText.getText().toString().length() == 0) {
-            mLongitudeEditText.setError("Longitude is required");
+        if (v == mPoweredByTextView) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://darksky.net/poweredby"));
+            startActivity(webIntent);
         }
-        else {
-            Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
-            intent.putExtra("latitude", latitude);
-            intent.putExtra("longitude", longitude);
-            startActivity(intent);
-        }
+
     }
+
 }
