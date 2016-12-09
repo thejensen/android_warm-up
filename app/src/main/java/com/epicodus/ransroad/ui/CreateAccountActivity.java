34535bab8831814040165456a68ccf35b6f1edbb
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-09 14:09:52 -0800

    send user to login screen from create account screen if they click the login link.

--- app/src/main/java/com/epicodus/ransroad/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/ransroad/ui/CreateAccountActivity.java
@@ -3,14 +3,30 @@ package com.epicodus.ransroad.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
 
-        Intent intent = getIntent();
+        mLoginTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLoginTextView) {
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
