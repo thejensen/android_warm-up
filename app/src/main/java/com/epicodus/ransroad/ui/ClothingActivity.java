37fc5b4664d6abe20117e7a31e2547b6d3835524
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-09 21:32:28 -0800

    use a recyclerview to populate list of clothing objects.

--- app/src/main/java/com/epicodus/ransroad/ui/ClothingActivity.java
+++ app/src/main/java/com/epicodus/ransroad/ui/ClothingActivity.java
@@ -3,6 +3,8 @@ package com.epicodus.ransroad.ui;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,22 +13,35 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.epicodus.ransroad.Constants;
+import com.epicodus.ransroad.adapter.ClothingListAdapter;
+import com.epicodus.ransroad.models.Clothing;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ClothingActivity extends AppCompatActivity{
+    @Bind(R.id.clothingRecyclerView) RecyclerView mClothingRecyclerView;
+    private ClothingListAdapter mAdapter;
+
+    public ArrayList<Clothing> mClothingItems = new ArrayList<>();
+
     private DatabaseReference mClothingItemReference;
 
     @Bind(R.id.clothingTitleTextView) TextView mClothingTitleTextView;
-    @Bind(R.id.clothingListView) ListView mClothingListView;
-    private String[] clothings = new String[] {"Front and Rear lights", "Fenders", "Water bottle", "Medium weight wool socks", "Lightweight boots",
-            "Long sleeve shirt or thin sweater", "Jeans", "Puffy jacket", "Gloves"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String[] clothings = new String[] {"Front and Rear lights", "Fenders", "Water bottle", "Medium weight wool socks", "Lightweight boots",
+                "Long sleeve shirt or thin sweater", "Jeans", "Puffy jacket", "Gloves"};
+        for (int i = 0; i < clothings.length; i++) {
+            Clothing item = new Clothing(clothings[i]);
+            mClothingItems.add(item);
+        }
+
         mClothingItemReference = FirebaseDatabase
                 .getInstance()
                 .getReference()
@@ -39,17 +54,24 @@ public class ClothingActivity extends AppCompatActivity{
         Typeface seasideFont = Typeface.createFromAsset(getAssets(), "fonts/seaside_font.ttf");
         mClothingTitleTextView.setTypeface(seasideFont);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, clothings);
-        mClothingListView.setAdapter(adapter);
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, clothings);
+//        mClothingListView.setAdapter(adapter);
 
-        mClothingListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String clothing = ((TextView)view).getText().toString();
-                Toast.makeText(ClothingActivity.this, clothing + " saved to 'Wish List'", Toast.LENGTH_LONG).show();
-                saveClothingItemToFirebase(clothing);
-            }
-        });
+        mAdapter = new ClothingListAdapter(getApplicationContext(), mClothingItems);
+        mClothingRecyclerView.setAdapter(mAdapter);
+        RecyclerView.LayoutManager layoutManager =
+                new LinearLayoutManager(ClothingActivity.this);
+        mClothingRecyclerView.setLayoutManager(layoutManager);
+        mClothingRecyclerView.setHasFixedSize(true);
+
+//        mClothingListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                String clothing = ((TextView)view).getText().toString();
+//                Toast.makeText(ClothingActivity.this, clothing + " saved to 'Wish List'", Toast.LENGTH_LONG).show();
+//                saveClothingItemToFirebase(clothing);
+//            }
+//        });
     }
 
     public void saveClothingItemToFirebase(String clothing) {
