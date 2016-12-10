c45d553cc1191bd89b7ff9a3c1d6673e1d3f0b7e
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-10 15:55:45 -0800

    install Picasso library, and update methods used by Picasso library to show images from clothing object in the image view of the clothing detail fragment.

--- app/src/main/java/com/epicodus/ransroad/ui/ClothingDetailFragment.java
+++ app/src/main/java/com/epicodus/ransroad/ui/ClothingDetailFragment.java
@@ -6,25 +6,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.epicodus.ransroad.R;
+import com.epicodus.ransroad.models.Clothing;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ClothingDetailFragment extends Fragment {
+    @Bind(R.id.addToWishListButton) Button mAddToWishListButton;
+    @Bind(R.id.clothingImageView) ImageView mImageLabel;
+    @Bind(R.id.clothingNameTextView) TextView mNameLabel;
+    @Bind(R.id.clothingDescriptionTextView) TextView mDescriptionLabel;
 
+    private Clothing mClothing;
 
-    public ClothingDetailFragment() {
-        // Required empty public constructor
+    public static ClothingDetailFragment newInstance(Clothing clothing) {
+        ClothingDetailFragment clothingDetailFragment = new ClothingDetailFragment();
+        Bundle args = new Bundle();
+        args.putParcelable("clothing", Parcels.wrap(clothing));
+        clothingDetailFragment.setArguments(args);
+        return clothingDetailFragment;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mClothing = Parcels.unwrap(getArguments().getParcelable("clothing"));
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_clothing_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_clothing_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        Picasso.with(view.getContext()).load(mClothing.getImageURL()).into(mImageLabel);
+
+        mNameLabel.setText(mClothing.getItem());
+        mDescriptionLabel.setText(mClothing.getDescription());
+
+        return view;
     }
 
 }
