896d009c298dd00509aff23311b9417cfe60845d
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-10 16:31:19 -0800

    set pager adapter to the clothing detail activity - it will unwrap the clothing arraylist parcelable and retrieve the position of the item in the list, so the detail activity defaults to the fragment view of the item clicked on by the user.

--- app/src/main/java/com/epicodus/ransroad/ui/ClothingDetailActivity.java
+++ app/src/main/java/com/epicodus/ransroad/ui/ClothingDetailActivity.java
@@ -1,13 +1,36 @@
 package com.epicodus.ransroad.ui;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.epicodus.ransroad.adapter.ClothingPagerAdapter;
+import com.epicodus.ransroad.models.Clothing;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class ClothingDetailActivity extends AppCompatActivity {
+    @Bind(R.id.viewPager)
+    ViewPager mViewPager;
+    private ClothingPagerAdapter adapterViewPager;
+    ArrayList<Clothing> mClothings = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothing_detail);
+        ButterKnife.bind(this);
+
+        mClothings = Parcels.unwrap(getIntent().getParcelableExtra("clothings"));
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewPager = new ClothingPagerAdapter(getSupportFragmentManager(), mClothings);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
     }
 }
