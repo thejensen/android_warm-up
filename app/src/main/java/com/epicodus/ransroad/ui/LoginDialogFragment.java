fe73d582333d06a77bbe2399e23daf57e3d0ec51
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-18 13:41:45 -0800

    successfully log in user from dialog fragment, save their firebase profile name to sharedPreferences.

--- app/src/main/java/com/epicodus/ransroad/ui/LoginDialogFragment.java
+++ app/src/main/java/com/epicodus/ransroad/ui/LoginDialogFragment.java
@@ -6,13 +6,24 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 
+import com.epicodus.ransroad.Constants;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,26 +32,45 @@ public class LoginDialogFragment extends DialogFragment {
     @Bind(R.id.emailDialogEditText) EditText mEmailDialogEditText;
     @Bind(R.id.passwordDialogEditText) EditText mPasswordDialogEditText;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         View view = View.inflate(getActivity(), R.layout.fragment_login_dialog, null);
         ButterKnife.bind(this, view);
 
+        mAuth = FirebaseAuth.getInstance();
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mEditor = mSharedPreferences.edit();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    addAuthStateToSharedPreferences(user.getDisplayName());
+                    Intent intent = new Intent(getActivity(), WishListActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+//                    finish();
+                }
+            }
+        };
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        // Get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
-        // Inflate and set the layout for the dialog
-        // Pass null as the parent view because its going in the dialog layout
         builder.setView(inflater.inflate(R.layout.fragment_login_dialog, null))
-                // Add action buttons
                 .setPositiveButton(R.string.signin, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
                         String email = mEmailDialogEditText.getText().toString();
                         String password = mPasswordDialogEditText.getText().toString();
-
-
+                        loginWithPassword(email, password);
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -50,4 +80,29 @@ public class LoginDialogFragment extends DialogFragment {
                 }).setView(view);
         return builder.create();
     }
+
+    private void loginWithPassword(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password);
+    }
+
+    private void addAuthStateToSharedPreferences(String name) {
+        Log.d(TAG, "add to shared prefs at login: " + name);
+        mEditor.putString(Constants.PREFERENCES_AUTHENTICATED, name);
+        mEditor.apply();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
 }
