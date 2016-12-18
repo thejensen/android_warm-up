640d295b8917902e4e9ae948af0f947a327ffe3d
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-17 21:08:52 -0800

    successfully bind views from login dialog layout to the login dialog fragment. phew.

--- app/src/main/java/com/epicodus/ransroad/ui/LoginDialogFragment.java
+++ app/src/main/java/com/epicodus/ransroad/ui/LoginDialogFragment.java
@@ -7,15 +7,27 @@ import android.app.DialogFragment;
 import android.app.Fragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoginDialogFragment extends DialogFragment {
+    @Bind(R.id.emailDialogEditText) EditText mEmailDialogEditText;
+    @Bind(R.id.passwordDialogEditText) EditText mPasswordDialogEditText;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View view = View.inflate(getActivity(), R.layout.fragment_login_dialog, null);
+        ButterKnife.bind(this, view);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         // Get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
@@ -27,7 +39,10 @@ public class LoginDialogFragment extends DialogFragment {
                 .setPositiveButton(R.string.signin, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                        // sign in the user ...
+                        String email = mEmailDialogEditText.getText().toString();
+                        String password = mPasswordDialogEditText.getText().toString();
+                        Log.v(TAG, "Email issss: " + email);
+                        Log.v(TAG, "Password issss: " + password);
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
